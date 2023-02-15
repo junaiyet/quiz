ae4c80bc9baa785f6b18cb8cbaa95d1ff7e275c0
junaiyetmia470@gmail.com <rezowan.7890@gmail.com>
2023-02-15 18:36:02 +0600

    code update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Header.css'
 import logo from '../../assets/logo.png'
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  const handScroll = () => {
+    if (window.scrollY >= 200) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
+
+  console.log(scrolled)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handScroll)
+    return () => {
+      window.removeEventListener("scroll", handScroll)
+    }
+  }, [])
   return (
 
-    <nav>
-        <div className="container navigation-area">
+
+    <nav className={scrolled ? "fixed" : null}>
+      <div className="container navigation-area">
         <div className="nav-logo">
-            <img src={logo} alt="" />
-           
+          <img src={logo} alt="" />
         </div>
         <div className="nav-items">
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/'>Quiz</NavLink>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/'>Quiz</NavLink>
         </div>
 
-        </div>
+      </div>
     </nav>
   )
 }
